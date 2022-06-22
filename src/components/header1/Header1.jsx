6d51fe87bc71d1a3480113c641140e5909e0a1df
isiagi <isiagigeofrey0@gmail.com>
2022-06-22 11:55:59 +0300

    changes txt on home show

--- src/components/header1/Header1.jsx
+++ src/components/header1/Header1.jsx
@@ -1,37 +1,41 @@
 import TypeAnimation from "react-type-animation";
 import "./header1.css";
-import {BsFillArrowDownCircleFill} from 'react-icons/bs'
+import { BsFillArrowDownCircleFill } from "react-icons/bs";
 
 const Header1 = () => {
   return (
     <div className="headerContainer">
       <div className="header__wrapper center">
-      <div className="header__div"/>
+        <div className="header__div" />
         <h1>MARITIME SHIPPING COMPANY</h1>
         <TypeAnimation
           className="animation"
           cursor={true}
           sequence={[
+            "SHIPPING",
+            2000,
+            "CLEARING AND FORWARDING",
+            2000,
+            "WARE HOUSING & DISTRIBUTION",
+            2000,
+            "LOGISTICS",
+            2000,
             "SEA FREIGT",
             2000,
             "AIR FREIGT",
             2000,
-            "WARE HOUSING",
-            2000,
-            "DISTRIBUTION & DELIVERY",
+            "INLAND TRANSPORT",
             2000,
             "CONTAINER LOAD & SHIPPING LINE",
             2000,
-            "INLAND TRANSPORT",
-            2000,
             "OIL, DANGEROUS CARGO & RELIEF LOGISTICS",
-            2000
+            2000,
           ]}
           wrapper="h4"
           repeat={Infinity}
         />
       </div>
-        <BsFillArrowDownCircleFill className="header__icon"/>
+      <BsFillArrowDownCircleFill className="header__icon" />
     </div>
   );
 };
